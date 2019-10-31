cb185fef2cf0be0336a3412b6e3a9e69c5908976
Gustavo Augusto <gustavolopes26@gmail.com>
2019-10-30 22:46:09 -0300

    Bugs in generation

--- Problem1.py
+++ Problem1.py
@@ -12,46 +12,7 @@ class Tree:
   def createNode(self, qnt_m1, qnt_c1, qnt_mb, qnt_cb, qnt_m2, qnt_c2):
     return Node(qnt_m1, qnt_c1, qnt_mb, qnt_cb, qnt_m2, qnt_c2)
 
-  def boarding(self, currentSide, currentNode):
-
-    #Verificar o lado atual se negativo lado 1 se positivo lado 2
-    if (not currentSide):
-
-      #Embarcar um Canibal e um Missionario 
-      if (currentNode.side1[0] - 1 >= (currentNode.side1[1] - 1) and currentNode.side1[0] - 1 >= 0 and currentNode.side1[1] - 1 >= 0):
-        print("Um de cada")
-        newNode = self.createNode(currentNode.side1[0]-1,currentNode.side1[1]-1,1,1,currentNode.side2[0],currentNode.side2[1])
-        currentNode.sons.append(newNode)
-
-      #Mandar somente um canibal
-      if (currentNode.side1[1] - 1 >= 0 and currentNode.side1[0] >= currentNode.side1[1] - 1):
-        print("Um canibal")
-        newNode = self.createNode(currentNode.side1[0],currentNode.side1[1]-1,0,1,currentNode.side2[0],currentNode.side2[1])
-        currentNode.sons.append(newNode)
-
-      #Mandar dois canibais 
-      if (currentNode.side1[1] - 2 >= 0 and currentNode.side1[0] >= currentNode.side1[1] - 2):
-        print("Dois canibais")
-        newNode = self.createNode(currentNode.side1[0],currentNode.side1[1]-2,0,2,currentNode.side2[0],currentNode.side2[1])
-        currentNode.sons.append(newNode)
-    else:
-
-      #Embarcar um Canibal e um Missionario 
-      if (currentNode.side2[0] - 1 >= currentNode.side2[1] - 1 and currentNode.side2[0] - 1 >= 0 and currentNode.side2[1] - 1 >= 0):
-        newNode = self.createNode(currentNode.side1[0],currentNode.side1[1],1,1,currentNode.side2[0]-1,currentNode.side2[1]-1)
-        currentNode.sons.append(newNode)
-      #Mandar somente um canibal
-      if (currentNode.side2[1] - 1 >= 0 and currentNode.side2[0] >= currentNode.side2[1] - 1):
-        newNode = self.createNode(currentNode.side1[0],currentNode.side1[1],0,1,currentNode.side2[0],currentNode.side2[1]-1)
-        currentNode.sons.append(newNode)
-      #Mandar dois canibais 
-      if (currentNode.side2[1] - 2 >= 0 and currentNode.side2[0] >= currentNode.side2[1] - 2):
-        newNode = self.createNode(currentNode.side1[0],currentNode.side1[1],0,2,currentNode.side2[0],currentNode.side2[1]-2)
-        currentNode.sons.append(newNode)
-  
-
-  def landing(self, currentSide, currentNode):
-
+  def print1(self, currentNode):
     print()
     print ("Lado 1: ")
     print()
@@ -61,7 +22,62 @@ class Tree:
     print("Destino: ")
     print("Qnt Missionario: ", currentNode.side2[0], "Qnt Canibal: ", currentNode.side2[1])
     print()
+
+  def boarding(self, currentSide, currentNode):
+
+    #Verificar o lado atual se negativo lado 1 se positivo lado 2
     if (not currentSide):
+      print("EMBARCAR NO LADO 1")
+      print()
+      self.print1(currentNode)
+      #Embarcar um Canibal e um Missionario 
+      if (currentNode.side1[0] - 1 >= (currentNode.side1[1] - 1) and currentNode.side1[0] - 1 >= 0 and currentNode.side1[1] - 1 >= 0):
+        print("Embarcar um de cada")
+        newNode = self.createNode(currentNode.side1[0]-1,currentNode.side1[1]-1,1,1,currentNode.side2[0],currentNode.side2[1])
+        currentNode.sons.append(newNode)
+
+      #Mandar somente um canibal
+      if (currentNode.side1[1] - 1 >= 0 and currentNode.side1[0] >= currentNode.side1[1] - 1):
+        print("Embarcar um canibal")
+        newNode = self.createNode(currentNode.side1[0],currentNode.side1[1]-1,0,1,currentNode.side2[0],currentNode.side2[1])
+        currentNode.sons.append(newNode)
+
+      #Mandar dois canibais 
+      if (currentNode.side1[1] - 2 >= 0 and currentNode.side1[0] >= currentNode.side1[1] - 2):
+        print("Embarcar dois canibais")
+        newNode = self.createNode(currentNode.side1[0],currentNode.side1[1]-2,0,2,currentNode.side2[0],currentNode.side2[1])
+        currentNode.sons.append(newNode)
+      print()
+    else:
+
+      print("EMBARCAR NO LADO 2")
+      print()
+      self.print1(currentNode)
+      #Embarcar um Canibal e um Missionario 
+      if (currentNode.side2[0] - 1 >= currentNode.side2[1] - 1 and currentNode.side2[0] - 1 >= 0 and currentNode.side2[1] - 1 >= 0):
+        print("Embarcar um de cada")
+        newNode = self.createNode(currentNode.side1[0],currentNode.side1[1],1,1,currentNode.side2[0]-1,currentNode.side2[1]-1)
+        currentNode.sons.append(newNode)
+      #Mandar somente um canibal
+      if (currentNode.side2[1] - 1 >= 0 and currentNode.side2[0] >= currentNode.side2[1] - 1):
+        print("Embarcar um canibal")
+        newNode = self.createNode(currentNode.side1[0],currentNode.side1[1],0,1,currentNode.side2[0],currentNode.side2[1]-1)
+        currentNode.sons.append(newNode)
+      #Mandar dois canibais 
+      if (currentNode.side2[1] - 2 >= 0 and currentNode.side2[0] >= currentNode.side2[1] - 2):
+        print("Embarcar dois canibais")
+        newNode = self.createNode(currentNode.side1[0],currentNode.side1[1],0,2,currentNode.side2[0],currentNode.side2[1]-2)
+        currentNode.sons.append(newNode)
+        print()
+
+  def landing(self, currentSide, currentNode):
+
+    
+    if (not currentSide):
+
+      print("DESEMBARCAR NO LADO 1")
+      print()
+      self.print1(currentNode)
       #Chegando um Missionario e um Canibal
       if (currentNode.boat[0] == 1 and currentNode.boat[1] == 1):
         
@@ -99,12 +115,13 @@ class Tree:
           print("É possivel descer um Missionário")
           newNode = self.createNode(currentNode.side1[0]+1,currentNode.side1[1],0,0,currentNode.side2[0], currentNode.side2[1])
           currentNode.sons.append(newNode)
-
+      print()
     else:
 
-      print("Descer no lado dois")
+      print("DESEMBARCAR NO LADO 2")
       print()
-       #Chegando um Missionario e um Canibal
+      self.print1(currentNode)
+        #Chegando um Missionario e um Canibal
       if (currentNode.boat[0] == 1 and currentNode.boat[1] == 1):
         
         #Verificar se é possível descer
@@ -138,14 +155,54 @@ class Tree:
           print("É possivel descer um Missionário")
           newNode = self.createNode(currentNode.side1[0],currentNode.side1[1],0,0,currentNode.side2[0]+1, currentNode.side2[1])
           currentNode.sons.append(newNode)
+      print()
 
+  def generateStates(self, currentNode, bord, land):
+
+    self.boarding(bord,currentNode)
+    #Verificar se é solução
+    #Percorrer os filhos do nó atual embarcados e gerar o estado de descer do barco
+    adjList = []
+    adjList.append(currentNode)
+    
+    while (len(adjList) != 0):
+      currentNode = adjList.pop(0)
+      for nodeBoarding in currentNode.sons:
+        #Para cada filho do nó atual, criar o estado de desembarque
+        print("Criando Estado de desembarque...")
+        self.landing(land,nodeBoarding)
+        #Percorrer os estados de desembarque para um novo embarque
+        for nodeLanding in nodeBoarding.sons:
+          print("EMBARCANDO....")
+          #Verificar se é solução
+          if (currentNode.side2[0] == 3 and currentNode.side2[1] == 3):
+            print("FINALIZADO")
+            return
+          else:
+            adjList.append(nodeLanding)
+            if (not bord): 
+              bord = True
+              land = False
+              self.boarding(bord,nodeLanding)
+              #self.generateStates(nodeLanding,True,False)
+            else:
+              bord = False
+              land = True
+              self.boarding(bord,nodeLanding)
+              #self.generateStates(nodeLanding,False,True)
+        print("PROXIMOOO")
+
+    
 
 tree = Tree()
 root = Node(3,3,0,0,0,0)
-tree.boarding(False,root)
-tree.landing(True,root.sons[0])
-tree.landing(True,root.sons[1])
-tree.landing(True,root.sons[2])
+
+tree.generateStates(root,False,True)
+#tree.boarding(False,root)
+#FALSE E TRUE NO PRIMEIRO
+#tree.landing(True,root.sons[0])
+#tree.landing(True,root.sons[1])
+#tree.landing(True,root.sons[2])
 """
 print()
 for node in root.sons:
